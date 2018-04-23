671aecbe11337f42a1e947f12a5cec2c6e4b87b1
fabiano <fabianoconrado@gmail.com>
2018-04-23 20:45:06 -0300

    Montando o projeto web

---
+++ DoorsWeb/DoorsWeb/App_Code/Colaborador.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Services;
+
+/// <summary>
+/// Summary description for Colaborador
+/// </summary>
+[WebService(Namespace = "http://cardexpressbrasil.web2302.uni5.net/")]
+[WebServiceBinding(ConformsTo = WsiProfiles.BasicProfile1_1)]
+// To allow this Web Service to be called from script, using ASP.NET AJAX, uncomment the following line. 
+public class Colaborador : System.Web.Services.WebService
+{
+
+    public Colaborador()
+    {
+
+        //Uncomment the following line if using designed components 
+        //InitializeComponent(); 
+    }
+
+    [WebMethod]
+    public DoorsExport.Model.Colaborador Obter(int empresa, int codigo)
+    {
+        using (var db = ConnectionDAO.GetInstancia().GetLiteConnection())
+        {
+            var arquivo = db.GetCollection<DoorsExport.Model.Colaborador>("colaboradores");
+
+            var result = arquivo.FindAll().Where(x => x.EMPRESA == empresa && x.CODIGO == codigo).FirstOrDefault();
+            return result;
+        }
+    }
+
+    [WebMethod]
+    public IList<DoorsExport.Model.Colaborador> ObterTodos(int empresa)
+    {
+        using (var db = ConnectionDAO.GetInstancia().GetLiteConnection())
+        {
+            var arquivo = db.GetCollection<DoorsExport.Model.Colaborador>("colaboradores");
+
+            var result = arquivo.FindAll().Where(x=>x.EMPRESA == empresa).ToList();
+            return result;
+        }
+    }
+
+}
---
+++ DoorsWeb/DoorsWeb/App_Code/ConnectionDAO.cs
@@ -0,0 +1,35 @@
+﻿using LiteDB;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Web;
+
+/// <summary>
+/// Summary description for ConnectionDAO
+/// </summary>
+public class ConnectionDAO
+{
+    public ConnectionDAO()
+    {
+    }
+    private static readonly ConnectionDAO instancia = new ConnectionDAO();
+
+    public static ConnectionDAO GetInstancia()
+    {
+        return instancia;
+    }
+
+    public IDbConnection GetMysqlConnection()
+    {
+        string conn = ConfigurationManager.ConnectionStrings["Firebird"].ToString();
+        return null;
+    }
+
+    public LiteDatabase GetLiteConnection()
+    {
+        string conn = ConfigurationManager.ConnectionStrings["LiteDB"].ToString();
+        return new LiteDatabase(conn);
+    }
+}
